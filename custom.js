e12444cc97976053a18df8bfb04ffc60863e27b2
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-12-06 18:28:52 +0530

    Two Task Completed

--- custom.js
+++ custom.js
@@ -5,5 +5,12 @@ module.exports ={
     output:{
         path : path.resolve(__dirname, 'dist'),
         filename : 'output.js' 
-    }
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 4500,
+      },
 }
